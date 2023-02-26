d30e4003d0dc5ab63195fb50d8578a9c2f192a7d
Alexander B <bas2784@gmail.com>
2023-02-26 23:25:37 +0300

    разные плюшки

--- js/script.js
+++ js/script.js
@@ -85,18 +85,89 @@ const chCount = (value) => {
 }
 
 const eventFunc = (e) => {
-    // chooseEl[i].className = "choosed_element"
-    if (e.target.className === "") {
-      e.target.className = "choosed_element"
-      chCount(1)
-    } else {
-        e.target.className = ""
-        chCount(-1)
-    }
+  // chooseEl[i].className = "choosed_element"
+  if (e.target.className === "") {
+    e.target.className = "choosed_element"
+    chCount(1)
+  } else {
+    e.target.className = ""
+    chCount(-1)
   }
+}
 
 for (i = 0; i < chooseEl.length; i++) {
   chooseEl[i].addEventListener('click', eventFunc)
 }
 
-chooseEl[2].removeEventListener('click', eventFunc)
+// chooseEl[2].removeEventListener('click', eventFunc)
+
+// const timeIsOver = () => {
+//   alert("Время вышло!")
+// }
+
+// setTimeout(timeIsOver, 2000)
+
+// const alarm = setInterval(timeIsOver, 3000)
+
+
+
+// const alarm = setInterval(() => {
+//   let wantToSleep =  confirm("Хотите ли вы спать?")
+//   if (wantToSleep) {
+//     console.log("tic")
+//   } else {
+//     clearInterval(alarm)
+//   }
+// }, 3000)
+
+const postsBlock = document.querySelector(".posts_block-container")
+const showPostsBtn = document.querySelector(".posts_block button")
+
+
+
+function addPost(title, body) {
+  const postTitle = document.createElement("h3")
+  const postBody = document.createElement("span")
+  const postItem = document.createElement("p")
+
+  postTitle.innerText = title
+  postBody.innerText = body
+
+  postItem.append(postTitle, postBody)
+  postsBlock.append(postItem)
+}
+
+function getPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+      for (item of data) {
+        addPost(item.title, item.body)
+      }
+      // addPost(data[7].title, data[7].body)
+    })
+    .catch(err => console.log(err.message))
+}
+
+// function createPost(title, body, userId) {
+//   fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       // title: title,
+//       // body: body,
+//       // userId: userId,
+//       title,
+//       body,
+//       userId,
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     }
+//   })
+//     .then(res => res.json())
+//     .catch(err => console.log(err.message))
+// }
+
+// createPost("title", "body", 15)
+
+showPostsBtn.onclick = () => {getPosts()}
